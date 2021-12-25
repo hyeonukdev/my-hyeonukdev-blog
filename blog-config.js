dd3e04ff83fa7441b20aa494f7181a6f986f9552
hyeonukdev <lego7407@gmail.com>
2021-12-25 14:54:46 +0900

    :wrench: Update my profile

--- blog-config.js
+++ blog-config.js
@@ -5,12 +5,10 @@ module.exports = {
   siteUrl: "https://hyeonukdev.github.io/my-hyeonukdev-blog",
   links: {
     github: "https://github.com/hyeonukdev",
-    // kaggle: "https://kaggle.com",
-    linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com/p",
-    instagram: "https://www.instagram.com/",
+    linkedIn: "https://www.linkedin.com/in/hyeonuk-kang-6b2085214/",
+    facebook: "https://www.facebook.com/lego7407",
     email: "mailto:lego7407@gmail.com",
-    etc: "https://www.notion.so/",
+    etc: "https://www.notion.so/5b768784d3064ef4a6bc92c291a8b9ef",
   },
   utterances: {
     repo: "hyeonukdev/my-hyeonukdev-blog",
