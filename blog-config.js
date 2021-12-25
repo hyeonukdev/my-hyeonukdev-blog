4fd2253205af33bf34160e4b4b744e76128f72c7
hyeonukdev <lego7407@gmail.com>
2021-12-25 14:56:53 +0900

    :wrench: Update siteURL

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "hyeonukdev-blog",
   description: "Hello :) I'm hyeonuk",
   author: "Hyeonukdev",
-  siteUrl: "https://hyeonukdev.github.io/my-hyeonukdev-blog",
+  siteUrl: "https://hyeonukdev.netlify.app",
   links: {
     github: "https://github.com/hyeonukdev",
     linkedIn: "https://www.linkedin.com/in/hyeonuk-kang-6b2085214/",
